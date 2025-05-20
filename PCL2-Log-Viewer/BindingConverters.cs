d665b861b734450d3049033f1bbf561104777b92
任天天 <ruattqwq@gmail.com>
2025-05-20 21:12:54 +0800

    refactor: use custom binding to reduce redundant codes

--- PCL2-Log-Viewer/BindingConverters.cs
+++ PCL2-Log-Viewer/BindingConverters.cs
@@ -1,19 +1,97 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.Globalization;
 using System.Linq;
+using System.Windows;
 using System.Windows.Data;
+using System.Windows.Markup;
 
 namespace LogViewer;
 
+// source: https://stackoverflow.com/a/51442634
 public class MultiValueEqualityConverter : IMultiValueConverter
 {
-    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    public object Convert(object?[]? values, Type targetType, object parameter, CultureInfo culture)
+        => values?.All(o => o?.Equals(values[0]) == true) == true || values?.All(o => o == null) == true;
+
+    public object[] ConvertBack(object? value, Type[] targetTypes, object parameter, CultureInfo culture)
     {
-        return values?.All(o => o?.Equals(values[0]) == true) == true || values?.All(o => o == null) == true;
+        throw new NotSupportedException();
+    }
+}
+
+public class ResourceKeyConverter : IValueConverter
+{
+    public string Prefix { get; set; } = "";
+    public string Suffix { get; set; } = "";
+    
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => $"{Prefix}{value}{Suffix}";
+    
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
+
+// source: claude-3-7-sonnet-20250219
+[ContentProperty(nameof(Items))]
+public class ItemExistenceConverter : IValueConverter
+{
+    public ObservableCollection<object> Items { get; } = [];
+    
+    private readonly HashSet<object> _hashSet = [];
+
+    public ItemExistenceConverter()
+    {
+        Items.CollectionChanged += (_, e) =>
+        {
+            if (e.NewItems != null) foreach (var item in e.NewItems) if (item != null) _hashSet.Add(item);
+            if (e.OldItems != null) foreach (var item in e.OldItems) if (item != null) _hashSet.Remove(item);
+            
+            if (e.Action == NotifyCollectionChangedAction.Reset)
+            {
+                _hashSet.Clear();
+                foreach (var item in Items) if (item != null) _hashSet.Add(item);
+            }
+        };
     }
 
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => value != null && _hashSet.Contains(value);
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
+
+// source: o4-mini
+public class ResourceLookupConverter : IMultiValueConverter
+{
+    public object Convert(object?[]? values, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (values == null || values.Length < 2)
+            throw new ArgumentException("Too few arguments");
+ 
+        var key = values[0];
+        if (key == null || values[1] is not FrameworkElement element)
+            return DependencyProperty.UnsetValue;
+
+        var resource = 
+#if DEBUG
+            element.FindResource(key);
+#else
+            element.TryFindResource(key);
+#endif
+        
+        return resource ?? DependencyProperty.UnsetValue;
+    }
+ 
     public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
     {
-        throw new NotImplementedException();
+        throw new NotSupportedException();
     }
 }
---
+++ PCL2-Log-Viewer/MarkupExtensions.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace LogViewer;
+
+// source: o4-mini | https://stackoverflow.com/a/28647821
+public class ResourceBinding : MultiBinding
+{
+    private static readonly ResourceLookupConverter ResourceLookupConverter = new();
+    
+    public ResourceBinding()
+    {
+        base.Converter = ResourceLookupConverter;
+        Bindings.Clear();
+        Bindings.Add(_binding);
+        Bindings.Add(new Binding { RelativeSource = new RelativeSource(RelativeSourceMode.Self) });
+    }
+
+    public ResourceBinding(PropertyPath path) : this()
+    {
+        Path = path;
+    }
+
+    #region Binding Members
+
+    private readonly Binding _binding = new();
+
+    /// <summary> The source path (for CLR bindings).</summary>
+    public object? Source { get => _binding.Source; set => _binding.Source = value; }
+
+    /// <summary> The source path (for CLR bindings).</summary>
+    public PropertyPath? Path { get => _binding.Path; set => _binding.Path = value; }
+
+    /// <summary> The XPath path (for XML bindings).</summary>
+    public string? XPath { get => _binding.XPath; set => _binding.XPath = value; }
+
+    /// <summary> The Converter to apply </summary>
+    public new IValueConverter? Converter { get => _binding.Converter; set => _binding.Converter = value; }
+
+    /// <summary>
+    /// The parameter to pass to converter.
+    /// </summary>
+    /// <value></value>
+    public new object? ConverterParameter { get => _binding.ConverterParameter; set => _binding.ConverterParameter = value; }
+
+    /// <summary> Culture in which to evaluate the converter </summary>
+    [TypeConverter(typeof(CultureInfoIetfLanguageTagConverter))]
+    public new CultureInfo? ConverterCulture { get => _binding.ConverterCulture; set => _binding.ConverterCulture = value; }
+
+    /// <summary>
+    /// Description of the object to use as the source, relative to the target element.
+    /// </summary>
+    public RelativeSource? RelativeSource { get => _binding.RelativeSource; set => _binding.RelativeSource = value; }
+
+    /// <summary> Name of the element to use as the source </summary>
+    public string? ElementName { get => _binding.ElementName; set => _binding.ElementName = value; }
+    
+    #endregion
+}
